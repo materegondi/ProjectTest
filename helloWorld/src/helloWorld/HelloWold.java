08ead25fb7564b76285c7e800bdeddcbc5107d7c
ProjectTest <matere.gonzalez@gmail.com>
2013-08-19 17:23:14 -0700

    adding hello world class

---
+++ helloWorld/src/helloWorld/HelloWold.java
@@ -0,0 +1,11 @@
+package helloWorld;
+
+public class HelloWold {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			System.out.print("Hello Wolrd tere");
+	}
+
+}
